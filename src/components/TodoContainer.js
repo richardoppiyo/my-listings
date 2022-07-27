a81511241115b9ad240a24c7742d461c48b9a321
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-28 00:35:10 +0300

    aadd delete functionality

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -24,21 +24,38 @@ class TodoContainer extends React.Component {
    };
 
   handleChange = (id) => {
-    this.setState({
-      todos: this.state.todos.map(todo => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
         if (todo.id === id) {
-          todo.completed = !todo.completed;
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
         }
-        return todo;
-      })
-    });
+        return todo
+      }),
+    }))
+  };
+
+  delTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    })
   };
 
   render() {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} handleChangeProps={this.handleChange}/>
+        <TodosList
+         todos={this.state.todos}
+         handleChangeProps={this.handleChange}
+         deleteTodoProps={this.delTodo}
+         />
     </div>
     )
   }
