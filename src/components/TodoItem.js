62de67dbb8dcf058903596c79ffa3cb9697a2b2b
Richard Opiyo <richardoppiyo@gmail.com>
2022-08-03 22:44:36 +0300

    add destrcuture of props

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,23 +2,31 @@ import React from "react";
 import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
-
-
-      
     render () {
-            return (
+
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#595959",
+            opacity: 0.4,
+            textDecoration: "line-through",
+          }
+        const { id } = this.props.todo
+        return (
             <li className={styles.item}>
-                <input 
-                 type="checkbox"
-                 checked={this.props.todo.completed} 
-                 onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-                 />
-                 <button
-                 onClick={() => this.props.deleteTodoProps(this.props.todo.id)}
-                 >Delete</button>
-                  {this.props.todo.title}
-                </li>
-            )
+              <input
+                type="checkbox"
+                className={styles.checkbox}
+                checked={this.props.todo.completed}
+                onChange={() => this.props.handleChangeProps(id)}
+              />
+              <button onClick={() => this.props.deleteTodoProps(id)}>
+                Delete
+              </button>
+              <span style={id.completed ? completedStyle : null}>
+                {this.props.todo.title}
+              </span>
+            </li>
+          )
     }
 }
 
