4aa800cd7d68906bbacf776ae23661bae9804f31
Richard Opiyo <richardoppiyo@gmail.com>
2022-08-07 22:41:10 +0300

    add lifecycle method

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -9,10 +9,27 @@ class TodoContainer extends React.Component {
     todos: []
    };
 
-   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
-      .then(response => response.json())
-      .then(data => this.setState({ todos: data }));
+  //  componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ todos: data }));
+  // }
+  
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+  }
+  
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
   }
 
   handleChange = (id) => {
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -16,7 +16,10 @@ class TodoItem extends React.Component {
               this.setState({ editing: false })
             }
           }
-
+          componentWillUnmount() {
+            console.log("Cleaning up...")
+          }
+          
     render () {
 
         let viewMode = {}
