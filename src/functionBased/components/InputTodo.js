8296cea1e857bd89bdf0056aff033a53bee34ceb
Richard Opiyo <richardoppiyo@gmail.com>
2022-08-07 23:14:54 +0300

    changing classes to functions

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,41 +1,7 @@
-import React, { Component } from "react"
+import React from "react"
 
-class InputTodo extends Component {
-  state = {
-    title: ""
-  };
-
-  onChange = e => {
-    this.setState({
-      title: e.target.value
-    });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    if (this.state.title.trim()) {
-      this.props.addTodoProps(this.state.title)
-      this.setState({
-        title: "",
-      })
-    } else {
-      alert("Please write item")
-    }
-  };
-  
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input 
-        type="text" 
-        placeholder="Add Todo..." 
-        value={this.state.title}
-        onChange={this.onChange}
-        />
-        <button>Submit</button>
-      </form>
-    )
-  }
+const InputTodo = () => {
+  return <div></div>
 }
+
 export default InputTodo
