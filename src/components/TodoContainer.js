0db1afad7d3e9296db74641acc88f73a0dc65930
Richard Opiyo <richardoppiyo@gmail.com>
2022-08-07 22:26:07 +0300

    added fapi fetchecd data

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -6,24 +6,14 @@ import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false
-      }
-    ]
+    todos: []
    };
+
+   componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(response => response.json())
+      .then(data => this.setState({ todos: data }));
+  }
 
   handleChange = (id) => {
     this.setState(prevState => ({
